95092d9c3c53cd929455cf48ec3a6e4068197754
KaziKoIzak <izak.benitezlopez@jacks.sdstate.edu>
2024-01-24 15:32:22 -0600

    hmmmm

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <chrono>
 
 using namespace std;
 
-vector<int> list = {3, 105, 3773, 13, 121, 78, 30751, 16461, 1233222, 348373443, 8769, 1011, 808, 121};
+int list[14] = {3, 105, 3773, 13, 121, 78, 30751, 16461, 1233222, 348373443, 8769, 1011, 808, 121};
 
 void multiple()
 {
-    for (int i = 0; i < list.size(); i++)
+    for (int i = 0; i < 14; i++)
     {
         if (list[i] % 1001 == 0)
             cout << "The number " << list[i] << " is a multiple of 7,11,13" << endl;
@@ -31,7 +33,7 @@ void multiple()
 void sum()
 {
     int number = 0;
-    for (int i = 0; i < list.size(); i++)
+    for (int i = 0; i < 14; i++)
     {
         int sum = 0;
         number = list[i];
@@ -58,7 +60,7 @@ void sum()
 void prime()
 {
     int number = 0;
-    for (int i = 0; i < list.size(); i++)
+    for (int i = 0; i < 14; i++)
     {
         bool answer = true;
 
@@ -82,7 +84,7 @@ void palindrome()
 {
     int number = 0;
 
-    for (int i = 0; i < list.size(); i++)
+    for (int i = 0; i < 14; i++)
     {
         int reversed = 0;
         int original = number = list[i];
@@ -108,12 +110,64 @@ void palindrome()
     }
 }
 
-int main()
+void arraySorter(vector<float> &sorting)
+{
+    int n = sorting.size();
+    bool swapped;
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        swapped = false;
+
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            if (sorting[j] > sorting[j + 1])
+            {
+                swap(sorting[j], sorting[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // If no swapping occurred in the inner loop, the array is already sorted
+        if (!swapped)
+        {
+            break;
+        }
+    }
+}
+
+vector<float> arrayMaker(vector<float> first, vector<float> second)
+{
+    vector<float> temp;
+
+    return temp;
+}
+
+void partA()
 {
     multiple();
     sum();
     prime();
     palindrome();
+}
+
+void partB()
+{
+    vector<float> firstOne = {3, 105, 3773, 13, 121, 78, 30751, 16461, 1233222, 8769, 1011, 808, 121};
+    vector<float> secondOne = {1.23, 1234.23, 34563, 5467.234, 9067.3432, -3452, -0053.345};
+
+    vector<float> biggerOne;
+
+    arraySorter(firstOne);
+    arraySorter(secondOne);
+
+    biggerOne = arrayMaker(firstOne, secondOne);
+}
+
+int main()
+{
+    partA();
+    partB();
 
     return 0;
 }

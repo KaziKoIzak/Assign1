f64f13425bf7e0edf9850a3cf23b6590861c0f2d
KaziKoIzak <izak.benitezlopez@jacks.sdstate.edu>
2024-01-30 20:21:33 -0600

    OOf

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
+// Library Inclusion
 #include <iostream>
 
 using namespace std;
 
+// Global Array
 int list[14] = {3, 105, 3773, 13, 121, 78, 30751, 16461, 1233222, 348373443, 8769, 1011, 808, 121};
+
+/*
+    Name: Multiple
+    In Args: None
+    Out Args: None
+    Definition: This function takes care of checking if the element in the array is a multiple of 7, 11, or
+    13. This is done by performing a mathematical operation with the modulo
+*/
 
 void multiple()
 {
+    // Loop to go through the array
     for (int i = 0; i < 14; i++)
     {
+        // If statement to check if the number is a multiple of either 7, 11, or 13
         if (list[i] % 1001 == 0)
             cout << "The number " << list[i] << " is a multiple of 7,11,13" << endl;
         else if (list[i] % 77 == 0)
@@ -27,6 +39,13 @@ void multiple()
     }
 }
 
+/*
+    Name: Sum
+    In Args: None
+    Out Args: None
+    Definition: This function takes care of checking if the element in the array has the sum of the digits
+    equal an odd or an even number.
+*/
 void sum()
 {
     int number = 0;
@@ -50,10 +69,19 @@ void sum()
 
         bool even = sum % 2;
 
+        // Tell the user
         cout << "The sum of the digits for " << list[i] << " is " << sum << " which is " << (even ? "Odd" : "Even") << endl;
     }
 }
 
+/*
+    Name: Prime
+    In Args: None
+    Out Args: None
+    Definition: This function takes care of checking if the element in the array is a prime or not. This is
+    done checking every number for 2 to the square root of 2 of that number. This is because the middle number
+    that can multiply to the number is the other number being a difference of 0 is the square root of that number
+*/
 void prime()
 {
     int number = 0;
@@ -73,10 +101,18 @@ void prime()
             }
         }
 
+        // Tell the user the results
         cout << "The Number " << number << " is" << (answer ? " a prime" : " not a prime") << endl;
     }
 }
 
+/*
+    Name: Palindrom
+    In Args: None
+    Out Args: None
+    Definition: This function takes care of checking if the element in the array is a palindrome by putting
+    the reversed number into a temporary number and checking the values later.
+*/
 void palindrome()
 {
     int number = 0;
@@ -103,16 +139,37 @@ void palindrome()
         if (original == reversed)
             answer = true;
 
+        // Tell the user if the number is a palindrome
         cout << "The Number " << reversed << " is" << (answer ? " a Palindrome of itself" : " not a Palindrom of itself") << endl;
     }
 }
 
+/*
+    Name: Print Array
+    In Args: Array and Size
+    Out Args: None
+    Definition: This function takes care of printing the elements of an array
+*/
 void printArray(float *arr, size_t size)
 {
+    // Go through every element of the array and print them
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
+/*
+    Name: Array Sorter
+    In Args: Array, Size
+    Out Args: None
+    Definition: This function takes care of using the bubble sort algorithm to sort the array from smallest
+    to largest.
+*/
 void arraySorter(float *arr, size_t size)
 {
+    // Bubble sort algorithm
     for (int i = 0; i < size - 1; ++i)
     {
         for (int j = 0; j < size - i - 1; ++j)
@@ -126,50 +183,105 @@ void arraySorter(float *arr, size_t size)
     }
 }
 
+/*
+    Name: Array Makeer
+    In Args: Array 1, Array 2, Array 3, size, size2
+    Out Args: None
+    Definition: This function takes care of combining the sorted arrays and merging them into one giant
+    array with the first half sorted to the first array and second half sorted with the second array
+*/
 void arrayMaker(float *arr, float *arr2, float *arr3, size_t size, size_t size2)
 {
     int i = 0;
+    // Put array 1 into larger array
     for (i = 0; i < size; i++)
     {
         arr3[i] = arr[i];
     }
+    // put array 2 into larger array
     for (int j = 0; j < size2; j++)
     {
-        arr3[i] = arr2[i];
+        arr3[i] = arr2[j];
         i++;
     }
 }
 
+/*
+    Name: Part A
+    In Args: None
+    Out Args: None
+    Definition: This function takes care of running Part A of the assignment
+*/
 void partA()
 {
+    // Print where we are at and then also print which function we are at
+    cout << "Part A of Assignment" << endl;
+    cout << "Multiple check of 7, 11, and 13" << endl;
     multiple();
+    cout << "\nSum Check if Odd or Even Digits" << endl;
     sum();
+    cout << "\nCheck if the number is a prime" << endl;
     prime();
+    cout << "\nCheck if the number is a palindrome" << endl;
     palindrome();
 }
 
+/*
+    Name: Part B
+    In Args: None
+    Out Args: None
+    Definition: This function takes care of running Part B of the assignment
+*/
 void partB()
 {
-    float system[] = {1, 2, 3, 4, 5, 6, 7, 12.4362, 24654.1234, 1234475.234, .00234923, .2341, 13, 14, 15};
-    float Programming[] = {.123, 13462234, 8756, 3456345, 3452, .2345237543, .74567, 2345634.3456, 48753.3456, 4356, 345345, 343562345};
+    cout << "\nPart B of the Assignment" << endl;
 
+    // Declare float arrays
+    float system[] = {1, 2, 3, 4, 5, 6, 7, 12.4362, 24654.1234, 1234475.234, .00234923, .2341, 13, 14, 15};
+    float Programming[] = {.123, 13462234, 8756, 3456345, 3452, .2345237543, .74567, 2345634.3456, 48753.3456, 4356, 345345, 695};
+
+    // Get the size of the array
     size_t size = sizeof(system) / sizeof(system[0]);
     size_t size2 = sizeof(Programming) / sizeof(Programming[0]);
+
+    // Print the arrays before they were sorted
+    cout << "Arrays Before Sorted" << endl;
 
     printArray(system, size);
     printArray(Programming, size2);
 
+    // Sort the array
     arraySorter(system, size);
     arraySorter(Programming, size2);
 
+    cout << "\nArrays After Sorted" << endl;
+
+    // Print the arrays after the sort
+    printArray(system, size);
+    printArray(Programming, size2);
+
+    // Get the size of the new array from combining the size of the previous two arrays
     size_t size3 = size + size2;
     float newArray[size3];
 
+    // Make the merged arary
     arrayMaker(system, Programming, newArray, size, size2);
+
+    cout << "\nMerged Array" << endl;
+
+    // Print the merged array
+    printArray(newArray, size3);
 }
 
+/*
+    Name: Main
+    In Args: None
+    Out Args: None
+    Definition: This is where the program beings
+*/
 int main()
 {
+    // Run part a then b
     partA();
     partB();
 
